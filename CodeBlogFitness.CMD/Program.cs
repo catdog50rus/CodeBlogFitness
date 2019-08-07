c4f18c33a0c8190db274e121fc5f17d6c35f1204
catdog50rus <catdog50rus@gmail.com>
2019-08-07 14:28:59 +0300

    07.08.19 14:28

---
+++ CodeBlogFitness.CMD/Program.cs
@@ -0,0 +1,32 @@
+﻿using CodeBlogFitness.BL.Model;
+using CodeBlogFitness.BL.Controller;
+using System;
+
+namespace CodeBlogFitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует приложение CodeBlogFitness");
+
+            Console.WriteLine("Введите имя пользователя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения");
+            var birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите вес");
+            var weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост");
+            var height = double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthDate, weight, height);
+            userController.Save();
+        }
+    }
+}
