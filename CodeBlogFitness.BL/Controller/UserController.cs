77f0411c712cdc5a9fd1ed16b6ddcaae3832effd
catdog50rus <catdog50rus@gmail.com>
2019-08-13 16:08:48 +0300

    13.08.19 16-10

--- CodeBlogFitness.BL/Controller/UserController.cs
+++ CodeBlogFitness.BL/Controller/UserController.cs
@@ -19,6 +19,8 @@ namespace CodeBlogFitness.BL.Controller
 
         public User CurrentUser { get; }
 
+        public bool ISNewUser { get; } = false;
+
         /// <summary>
         /// Создание нового контроллера
         /// </summary>
@@ -36,11 +38,12 @@ namespace CodeBlogFitness.BL.Controller
             {
                 CurrentUser = new User(userName);
                 Users.Add(CurrentUser);
+                ISNewUser = true;
                 Save();
             }
 
-            var gender = new Gender(genderName);
-            Users = new User(userName, gender, birthDate, weight, height);
+            //var gender = new Gender(genderName);
+            //Users = new User(userName, gender, birthDate, weight, height);
             
         }
         /// <summary>
@@ -65,16 +68,27 @@ namespace CodeBlogFitness.BL.Controller
 
         }
 
+        public void SetNewUserData(string genderName, DateTime birthDate, double weight = 1, double height = 1)
+        {
+            // TODO Проверка
+
+            CurrentUser.Gender = new Gender(genderName);
+            CurrentUser.BirthDate = birthDate;
+            CurrentUser.Weight = weight;
+            CurrentUser.Height = height;
+            Save();
+        }
+
         /// <summary>
         /// Сохранить данный пользователя
         /// </summary>
         /// <param name="user"></param>
-        private void Save()
+        public void Save()
         {
             var formatter = new BinaryFormatter();
             using(var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                formatter.Serialize(fs, User);
+                formatter.Serialize(fs, Users);
             }            
         }
 
